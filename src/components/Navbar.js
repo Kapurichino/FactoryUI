d51aefd7a41744c3f3217c2954c9bf20e75142bf
Bang SungWon <bsw9698@gmail.com>
2022-08-11 23:38:25 +0900

    사이드바 서브메뉴 구현  * 사이드바의 서브메뉴를 구현하였습니다. 다만, 구현 구조가 너무    하드코딩이며, 추가 유지보수 비용이 많이 들 것이라 판단되어, 좀더    수정할 예정입니다.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { faBook, faCircleInfo, faMagnifyingGlass, faUserTie, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faBook, faCaretDown, faCircleInfo, faMagnifyingGlass, faUserTie, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Navbar.css";
 import { useState } from "react";
@@ -6,28 +6,83 @@ import { faElementor } from "@fortawesome/free-brands-svg-icons";
 
 export default function Navbar(){
     const [show, setShow] = useState("");
+    const [down, setDown] = useState(["", "", "", ""]);
     return(
         <>
-            <FontAwesomeIcon icon={faElementor} className={"openIcon"} onClick={()=>{setShow("showMenu")}}/>
-            
+            <FontAwesomeIcon icon={faElementor} className={"openIcon"} onClick={()=>{setShow("showMenu")}}/>  
             <div className={`menu ${show}`}>
                 <div className={"menuBar"}>
                     <div className={"menuContent"}>
                         <FontAwesomeIcon icon={faXmark} className={"closeIcon"} onClick={()=>{setShow("")}}/>
-                        <ul>
-                            <li><span><FontAwesomeIcon icon={faCircleInfo}/> 정보</span></li>
-                            <li><span><FontAwesomeIcon icon={faMagnifyingGlass}/> 서비스</span></li>
-                            <li><span><FontAwesomeIcon icon={faBook}/> 스토리</span></li>
-                            <li><span><FontAwesomeIcon icon={faUserTie}/> 약속</span></li>
-                            <li><span>↑ SAMPLE</span></li>
+                        <ul className={down[0]}>
+                            <li onClick={()=>{
+                                if (down[0] == ""){
+                                setDown(["down", "", "", ""]);
+                                }
+                                else{
+                                    let copy = [...down];
+                                    copy[0] = "";
+                                    setDown(copy);
+                                }
+                            }}><span><FontAwesomeIcon icon={faCircleInfo}/>정보<FontAwesomeIcon icon={faCaretDown}/></span></li>
+                            <li><span>SAMPLE</span></li>
+                            <li><span>SAMPLE</span></li>
+                            <li><span>SAMPLE</span></li>
+                            <li><span>SAMPLE</span></li>
+                        </ul>
+                        <ul className={down[1]}>
+                            <li onClick={()=>{
+                                if (down[1] == ""){
+                                setDown(["", "down", "", ""]);
+                                }
+                                else{
+                                    let copy = [...down];
+                                    copy[1] = "";
+                                    setDown(copy);
+                                }
+                            }}><span><FontAwesomeIcon icon={faMagnifyingGlass}/>서비스<FontAwesomeIcon icon={faCaretDown}/></span></li>
+                            <li><span>SAMPLE</span></li>
+                            <li><span>SAMPLE</span></li>
+                            <li><span>SAMPLE</span></li>
+                            <li><span>SAMPLE</span></li>
+                        </ul>
+                        <ul className={down[2]}>
+                            <li onClick={()=>{
+                                if (down[2] == ""){
+                                setDown(["", "", "down", ""]);
+                                }
+                                else{
+                                    let copy = [...down];
+                                    copy[2] = "";
+                                    setDown(copy);
+                                }
+                            }}><span><FontAwesomeIcon icon={faBook}/>스토리<FontAwesomeIcon icon={faCaretDown}/></span></li>
+                            <li><span>SAMPLE</span></li>
+                            <li><span>SAMPLE</span></li>
+                            <li><span>SAMPLE</span></li>
+                            <li><span>SAMPLE</span></li>
+                        </ul>
+                        <ul className={down[3]}>
+                            <li onClick={()=>{
+                                if (down[3] == ""){
+                                setDown(["", "", "", "down"]);
+                                }
+                                else{
+                                    let copy = [...down];
+                                    copy[3] = "";
+                                    setDown(copy);
+                                }
+                            }}><span><FontAwesomeIcon icon={faUserTie}/>약속<FontAwesomeIcon icon={faCaretDown}/></span></li>
+                            <li><span>SAMPLE</span></li>
+                            <li><span>SAMPLE</span></li>
+                            <li><span>SAMPLE</span></li>
+                            <li><span>SAMPLE</span></li>
                         </ul>
                     </div>
                 </div>
-                <div className={"overlay"}/>   
+                <div className={"overlay"} onClick={()=>{setShow("")}}/>   
             </div>
-            
-           
-            
+      
             <div className={"navBar"}>
                 <div className={"navContent"}>
                     <ul>
